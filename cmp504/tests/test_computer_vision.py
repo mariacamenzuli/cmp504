eea9eaae7f9447966e09d612c0cc65e2a37af56f
Maria Camenzuli <maria.camenzuli93@gmail.com>
2019-06-24 22:04:14 +0100

    Adding test for template match including transparency

--- cmp504/tests/test_computer_vision.py
+++ cmp504/tests/test_computer_vision.py
@@ -62,9 +62,11 @@ def test_find_template_match_cross_correlation_when_match_does_not_exist():
     assert_that(result).is_none()
 
 
-# def test_capture_frame():
-#     computer_vision_ctrl = cmp504.computer_vision.CVController()
-#     computer_vision_ctrl.capture_frame()
-#     result = computer_vision_ctrl.find_template_match("cmp504/data/test/wargroove_cherrystone_stronghold.png")
-#
-#     assert_that(result).is_true()
+def test_find_template_match_with_transparent_template_when_match_exists():
+    cv_ctrl = cmp504.computer_vision.CVController()
+    cv_ctrl.load_frame("cmp504/data/test/wargroove_screenshot.png")
+    result = cv_ctrl.find_template_match("cmp504/data/test/wargroove_cherrystone_stronghold_with_transparency.png",
+                                         threshold=0.8,
+                                         method=cmp504.computer_vision.TemplateMatchingMethod.CROSS_CORRELATION_NORMALIZED)
+
+    assert_that(result).is_not_none()
