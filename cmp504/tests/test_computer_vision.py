137671b1708484e557c2e2fc059b5fd48e6cad60
Maria Camenzuli <maria.camenzuli93@gmail.com>
2019-07-01 22:32:21 +0100

    Making tests for template matching more specific

--- cmp504/tests/test_computer_vision.py
+++ cmp504/tests/test_computer_vision.py
@@ -5,71 +5,83 @@ from assertpy import assert_that
 def test_find_template_match_square_difference_when_match_exists():
     cv_ctrl = cmp504.computer_vision.CVController()
     cv_ctrl.load_frame("cmp504/data/test/wargroove_screenshot.png")
-    result = cv_ctrl.find_template_match("cmp504/data/test/wargroove_cherrystone_stronghold.png",
-                                         threshold=0.2,
-                                         method=cmp504.computer_vision.TemplateMatchingMethod.SQUARE_DIFFERENCE_NORMALIZED)
+    match = cv_ctrl.find_template_match("cmp504/data/test/wargroove_cherrystone_stronghold.png",
+                                        threshold=0.2,
+                                        method=cmp504.computer_vision.TemplateMatchingMethod.SQUARE_DIFFERENCE_NORMALIZED)
 
-    assert_that(result).is_not_none()
+    assert_that(match).is_not_none()
+    assert_that(match.top_left).is_equal_to((246, 303))
+    assert_that(match.bottom_right).is_equal_to((310, 389))
+    assert_that(match.mid_point).is_equal_to((278, 346))
 
 
 def test_find_template_match_square_difference_when_match_does_not_exist():
     cv_ctrl = cmp504.computer_vision.CVController()
     cv_ctrl.load_frame("cmp504/data/test/wargroove_screenshot.png")
-    result = cv_ctrl.find_template_match("cmp504/data/test/discord_icon.png",
-                                         threshold=0.2,
-                                         method=cmp504.computer_vision.TemplateMatchingMethod.SQUARE_DIFFERENCE_NORMALIZED)
+    match = cv_ctrl.find_template_match("cmp504/data/test/discord_icon.png",
+                                        threshold=0.2,
+                                        method=cmp504.computer_vision.TemplateMatchingMethod.SQUARE_DIFFERENCE_NORMALIZED)
 
-    assert_that(result).is_none()
+    assert_that(match).is_none()
 
 
 def test_find_template_match_correlation_coefficient_when_match_exists():
     cv_ctrl = cmp504.computer_vision.CVController()
     cv_ctrl.load_frame("cmp504/data/test/wargroove_screenshot.png")
-    result = cv_ctrl.find_template_match("cmp504/data/test/wargroove_cherrystone_stronghold.png",
+    match = cv_ctrl.find_template_match("cmp504/data/test/wargroove_cherrystone_stronghold.png",
                                          threshold=0.8,
                                          method=cmp504.computer_vision.TemplateMatchingMethod.CORRELATION_COEFFICIENT_NORMALIZED)
 
-    assert_that(result).is_not_none()
+    assert_that(match).is_not_none()
+    assert_that(match.top_left).is_equal_to((246, 303))
+    assert_that(match.bottom_right).is_equal_to((310, 389))
+    assert_that(match.mid_point).is_equal_to((278, 346))
 
 
 def test_find_template_match_correlation_coefficient_when_match_does_not_exist():
     cv_ctrl = cmp504.computer_vision.CVController()
     cv_ctrl.load_frame("cmp504/data/test/wargroove_screenshot.png")
-    result = cv_ctrl.find_template_match("cmp504/data/test/discord_icon.png",
-                                         threshold=0.8,
-                                         method=cmp504.computer_vision.TemplateMatchingMethod.CORRELATION_COEFFICIENT_NORMALIZED)
+    match = cv_ctrl.find_template_match("cmp504/data/test/discord_icon.png",
+                                        threshold=0.8,
+                                        method=cmp504.computer_vision.TemplateMatchingMethod.CORRELATION_COEFFICIENT_NORMALIZED)
 
-    assert_that(result).is_none()
+    assert_that(match).is_none()
 
 
 def test_find_template_match_cross_correlation_when_match_exists():
     cv_ctrl = cmp504.computer_vision.CVController()
     cv_ctrl.load_frame("cmp504/data/test/wargroove_screenshot.png")
-    result = cv_ctrl.find_template_match("cmp504/data/test/wargroove_cherrystone_stronghold.png",
-                                         threshold=0.8,
-                                         method=cmp504.computer_vision.TemplateMatchingMethod.CROSS_CORRELATION_NORMALIZED)
+    match = cv_ctrl.find_template_match("cmp504/data/test/wargroove_cherrystone_stronghold.png",
+                                        threshold=0.8,
+                                        method=cmp504.computer_vision.TemplateMatchingMethod.CROSS_CORRELATION_NORMALIZED)
 
-    assert_that(result).is_not_none()
+    assert_that(match).is_not_none()
+    assert_that(match.top_left).is_equal_to((246, 303))
+    assert_that(match.bottom_right).is_equal_to((310, 389))
+    assert_that(match.mid_point).is_equal_to((278, 346))
 
 
 def test_find_template_match_cross_correlation_when_match_does_not_exist():
     cv_ctrl = cmp504.computer_vision.CVController()
     cv_ctrl.load_frame("cmp504/data/test/wargroove_screenshot.png")
-    result = cv_ctrl.find_template_match("cmp504/data/test/discord_icon.png",
-                                         threshold=0.8,
-                                         method=cmp504.computer_vision.TemplateMatchingMethod.CROSS_CORRELATION_NORMALIZED)
+    match = cv_ctrl.find_template_match("cmp504/data/test/discord_icon.png",
+                                        threshold=0.8,
+                                        method=cmp504.computer_vision.TemplateMatchingMethod.CROSS_CORRELATION_NORMALIZED)
 
-    assert_that(result).is_none()
+    assert_that(match).is_none()
 
 
 def test_find_template_match_with_transparent_template_when_match_exists():
     cv_ctrl = cmp504.computer_vision.CVController()
     cv_ctrl.load_frame("cmp504/data/test/wargroove_screenshot.png")
-    result = cv_ctrl.find_template_match("cmp504/data/test/wargroove_cherrystone_stronghold_with_transparency.png",
-                                         threshold=0.8,
-                                         method=cmp504.computer_vision.TemplateMatchingMethod.CROSS_CORRELATION_NORMALIZED)
+    match = cv_ctrl.find_template_match("cmp504/data/test/wargroove_cherrystone_stronghold_with_transparency.png",
+                                        threshold=0.8,
+                                        method=cmp504.computer_vision.TemplateMatchingMethod.CROSS_CORRELATION_NORMALIZED)
 
-    assert_that(result).is_not_none()
+    assert_that(match).is_not_none()
+    assert_that(match.top_left).is_equal_to((246, 303))
+    assert_that(match.bottom_right).is_equal_to((310, 389))
+    assert_that(match.mid_point).is_equal_to((278, 346))
 
 
 def test_find_text_in_frame_with_no_preprocessing():
@@ -84,6 +96,7 @@ def test_find_text_in_frame_with_no_preprocessing():
     result = cv_ctrl.find_text()
 
     assert_that(result).is_equal_to("650 3428")
+
 
 def test_find_text_in_frame_with_preprocessing():
     cv_ctrl = cmp504.computer_vision.CVController()
