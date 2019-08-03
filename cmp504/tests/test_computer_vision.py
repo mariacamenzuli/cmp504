82743defc0c71004e7dd6bc2dd4e7fdfc12eaeca
Maria Camenzuli <maria.camenzuli93@gmail.com>
2019-08-03 21:59:41 +0100

    Added a new hu moment based template match method
    Uses a custom matching method that should perform better when an object is mirrored horizontally

--- cmp504/tests/test_computer_vision.py
+++ cmp504/tests/test_computer_vision.py
@@ -199,6 +199,30 @@ def test_find_template_match_hu_moments_finds_mirrored_match():
     assert_that(match.mid_point).is_equal_to((1226, 50))
 
 
+def test_find_template_match_hu_moments_custom_finds_untransformed_match():
+    cv_ctrl = cmp504.computer_vision.CVController()
+    cv_ctrl.load_frame("cmp504/data/test/wargroove_screenshot.png")
+    match = cv_ctrl.find_template_match_hu_moments_custom("cmp504/data/test/wargroove_commander_portrait.png",
+                                                          binarization_threshold=200)
+
+    assert_that(match).is_not_none()
+    assert_that(match.top_left).is_equal_to((13, 13))
+    assert_that(match.bottom_right).is_equal_to((84, 86))
+    assert_that(match.mid_point).is_equal_to((48, 50))
+
+
+def test_find_template_match_hu_moments_custom_finds_mirrored_match():
+    cv_ctrl = cmp504.computer_vision.CVController()
+    cv_ctrl.load_frame("cmp504/data/test/wargroove_screenshot_mirrored_commander.png")
+    match = cv_ctrl.find_template_match_hu_moments_custom("cmp504/data/test/wargroove_commander_portrait.png",
+                                                          binarization_threshold=200)
+
+    assert_that(match).is_not_none()
+    assert_that(match.top_left).is_equal_to((1190, 13))
+    assert_that(match.bottom_right).is_equal_to((1261, 86))
+    assert_that(match.mid_point).is_equal_to((1226, 50))
+
+
 def test_find_best_feature_based_match_sift():
     cv_ctrl = cmp504.computer_vision.CVController()
     cv_ctrl.load_frame("cmp504/data/test/wargroove_screenshot.png")
